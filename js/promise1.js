4e2808e3136bbc84abd020c013efd03aa44cace8
clothn0105700 <clothn0105700@gmail.com>
2022-12-08 23:35:32 +0800

    完成promise1.js 修改callbacke1.js

---
+++ js/promise1.js
@@ -0,0 +1,23 @@
+let doWorkPromise = function(job, timer){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            let now = new Date();
+            resolve(`完成工作 ${job} at ${now.toISOString()}`);
+        }, timer);
+    })
+  }
+  
+  let now = new Date();
+  console.log(`工作開始 at ${now.toISOString()}`);
+  
+  brushPromise
+    .then((data) =>{
+        console.log(data);
+        return doWorkPromise(`吃早餐`, 5000);
+    }).then((data) =>{
+        console.log(data);
+        return doWorkPromise(`寫功課`, 3000);
+    }).then((data) =>{
+        console.log(data);
+    })
+ 
