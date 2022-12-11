689029773f92bcc11a060bf5db0c51919c268900
clothn0105700 <clothn0105700@gmail.com>
2022-12-11 15:25:16 +0800

    修正crawker2.js

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 
 (async() => {
     try{
-   let response = await axios
+    let response = await axios
     .get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211')
     console.log(response.data);
     } catch(err){
